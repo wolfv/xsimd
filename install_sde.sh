2c5bb08ad3c6e369ac7b8307973c2e9ea3826991
Wolf Vollprecht <w.vollprecht@gmail.com>
2018-05-31 11:38:01 +0200

    install sde in travis

---
+++ install_sde.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/marehr/intel-sde-downloader
+cd intel-sde-downloader
+pip install -r requirements.txt
+python ./intel-sde-downloader.py sde-external-8.16.0-2018-01-30-lin.tar.bz2
+tar xvf sde-external-8.16.0-2018-01-30-lin.tar.bz2
